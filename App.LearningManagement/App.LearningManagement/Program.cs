fd057a47835a1aaa98fa58d9e1f24ee62fbf3e08
crmills <crmills@fsu.edu>
2023-02-02 09:17:32 -0500

    Initial Commit - Closes #4

---
+++ App.LearningManagement/App.LearningManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ App.LearningManagement/Library.LearningManagement/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.LearningManagement.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; }
+
+        public char Classification { get; set; }
+
+        public Person()
+        {
+            Name = string.Empty;
+            Grades = new Dictionary<int, double>();
+        }
+    }
+}
