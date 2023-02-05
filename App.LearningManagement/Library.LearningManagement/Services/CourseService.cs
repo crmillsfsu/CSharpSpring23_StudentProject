8b7156d5d4963e25a90feb1d0ccfe33c255c618f
crmills <crmills@fsu.edu>
2023-02-04 22:20:22 -0500

    Added search functionality for courses Closes #11

--- App.LearningManagement/Library.LearningManagement/Services/CourseService.cs
+++ App.LearningManagement/Library.LearningManagement/Services/CourseService.cs
@@ -9,7 +9,7 @@ namespace Library.LearningManagement.Services
 {
     public class CourseService
     {
-        public List<Course> courseList = new List<Course>();
+        private List<Course> courseList = new List<Course>();
 
         public void Add(Course course)
         {
@@ -22,6 +22,13 @@ namespace Library.LearningManagement.Services
             {
                 return courseList;
             }
+        }
+
+        public IEnumerable<Course> Search(string query)
+        {
+            return Courses.Where(s => s.Name.ToUpper().Contains(query.ToUpper())
+                || s.Description.ToUpper().Contains(query.ToUpper())
+                || s.Code.ToUpper().Contains(query.ToUpper()));
         }
 
     }
