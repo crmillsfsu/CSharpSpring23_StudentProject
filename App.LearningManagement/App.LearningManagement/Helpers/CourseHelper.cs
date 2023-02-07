f12d77c625d9ed89b72c5f91220e191724432ec4
crmills <crmills@fsu.edu>
2023-02-06 21:19:47 -0500

    commit initial changes for enrolling a student in a course

--- App.LearningManagement/App.LearningManagement/Helpers/CourseHelper.cs
+++ App.LearningManagement/App.LearningManagement/Helpers/CourseHelper.cs
@@ -11,6 +11,7 @@ namespace App.LearningManagement.Helpers
     public class CourseHelper
     {
         private CourseService courseService = new CourseService();
+        private StudentService studentService = new StudentService();
 
         public void CreateCourseRecord(Course? selectedCourse = null)
         {
@@ -20,6 +21,30 @@ namespace App.LearningManagement.Helpers
             var name = Console.ReadLine() ?? string.Empty;
             Console.WriteLine("What is the description of the course?");
             var description = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Which students should be enrolled in this course? ('Q' to quit)");
+            var roster = new List<Person>();
+            bool continueAdding = true;
+            while(continueAdding)
+            {
+                studentService.Students.Where(s => !roster.Any(s2 => s2.Id == s.Id)).ToList().ForEach(Console.WriteLine);
+                var selection = Console.ReadLine() ?? string.Empty;
+
+                if(selection.Equals("Q", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continueAdding = false;
+                } else
+                {
+                    var selectedId = int.Parse(selection);
+                    var selectedStudent = studentService.Students.FirstOrDefault(s => s.Id == selectedId);
+
+                    if(selectedStudent != null)
+                    {
+                        roster.Add(selectedStudent);
+                    }
+                }
+            }
+
 
             bool isNewCourse = false;
             if(selectedCourse == null)
@@ -31,6 +56,8 @@ namespace App.LearningManagement.Helpers
             selectedCourse.Code = code;
             selectedCourse.Name = name;
             selectedCourse.Description = description;
+            selectedCourse.Roster = new List<Person>();
+            selectedCourse.Roster.AddRange(roster);
 
             if(isNewCourse)
             {
