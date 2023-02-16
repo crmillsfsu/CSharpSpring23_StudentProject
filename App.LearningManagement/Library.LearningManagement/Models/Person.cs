999cdf28673ded137233855fd36ac506232905eb
crmills <crmills@fsu.edu>
2023-02-16 08:59:49 -0500

    Adding Student derived class. Closes #40

--- App.LearningManagement/Library.LearningManagement/Models/Person.cs
+++ App.LearningManagement/Library.LearningManagement/Models/Person.cs
@@ -5,19 +5,14 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Dictionary<int, double> Grades { get; set; }
-
-        public PersonClassification Classification { get; set; }
-
         public Person()
         {
             Name = string.Empty;
-            Grades = new Dictionary<int, double>();
         }
 
         public override string ToString()
         {
-            return $"[{Id}] {Name} - {Classification}";
+            return $"[{Id}] {Name}";
         }
     }
 
