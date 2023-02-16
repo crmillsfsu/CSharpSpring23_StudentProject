e54eec4b9a428e5b43ab5caf5cc79cdb68301bfb
crmills <crmills@fsu.edu>
2023-02-16 09:29:31 -0500

    Added PageItem. Closes #33

---
+++ App.LearningManagement/Library.LearningManagement/Models/PageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.LearningManagement.Models
+{
+    public class PageItem : ContentItem
+    {
+        public string? HtmlBody { get; set; }
+    }
+}
