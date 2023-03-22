8f07c42905b66998e1449ca43d950180c2e5b8cd
crmills <crmills@fsu.edu>
2023-03-21 22:11:48 -0400

    Added Id and Code uniqueness for Courses. Closes #55

--- App.LearningManagement/Library.LearningManagement/Models/Course.cs
+++ App.LearningManagement/Library.LearningManagement/Models/Course.cs
@@ -8,7 +8,18 @@ namespace Library.LearningManagement.Models
 {
     public class Course
     {
-        public string Code { get; set; }
+        public string Code { 
+            get
+            {
+                return $"{Prefix}{Id}";
+            }
+        }
+        private static int lastId = 0;
+        public string Prefix { get; set; }
+        public int Id
+        {
+            get; private set;
+        }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Person> Roster { get; set; }
@@ -16,12 +27,13 @@ namespace Library.LearningManagement.Models
         public List<Module> Modules { get; set; }
 
         public Course() { 
-            Code = string.Empty;
             Name = string.Empty;
             Description = string.Empty;
             Roster= new List<Person>();
             Assignments= new List<Assignment>();
             Modules= new List<Module>();
+            Prefix = string.Empty;
+            Id = ++lastId;
         }
 
         public override string ToString()
