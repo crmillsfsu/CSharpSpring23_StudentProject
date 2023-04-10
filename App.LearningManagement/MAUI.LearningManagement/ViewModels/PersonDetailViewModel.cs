5b09dcd2ea24d2c30b12b5c44f6a94b26ae868f6
crmills <crmills@fsu.edu>
2023-04-09 21:27:06 -0400

    Added BindingContext reset on navigating to and from the person detail view. Closes #95

--- App.LearningManagement/MAUI.LearningManagement/ViewModels/PersonDetailViewModel.cs
+++ App.LearningManagement/MAUI.LearningManagement/ViewModels/PersonDetailViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.LearningManagement.Models;
+using Library.LearningManagement.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,28 @@ namespace MAUI.LearningManagement.ViewModels
     {
         public string Name { get; set; }
         public string ClassificationString { get; set; }
+
+        public void AddPerson()
+        {
+            PersonClassification classification;
+            switch (ClassificationString)
+            {
+                case "S":
+                    classification = PersonClassification.Senior;
+                    break;
+                case "J":
+                    classification = PersonClassification.Junior;
+                    break;
+                case "O":
+                    classification = PersonClassification.Sophomore;
+                    break;
+                case "F":
+                default:
+                    classification = PersonClassification.Freshman;
+                    break;
+            }
+            StudentService.Current.Add(new Student { Name = Name, Classification = classification });
+            Shell.Current.GoToAsync("//Instructor");
+        }
     }
 }
--- App.LearningManagement/MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
+++ App.LearningManagement/MAUI.LearningManagement/Views/PersonDetailView.xaml.cs
@@ -9,32 +9,21 @@ public partial class PersonDetailView : ContentPage
 	public PersonDetailView()
 	{
 		InitializeComponent();
-
-		BindingContext = new PersonDetailViewModel();
 	}
 
     private void OkClick(object sender, EventArgs e)
     {
 		//TODO: Need to migrate this to the ViewModel
-		var context = BindingContext as PersonDetailViewModel;
-		PersonClassification classification;
-		switch (context.ClassificationString)
-		{
-			case "S":
-				classification = PersonClassification.Senior;
-				break;
-			case "J":
-				classification = PersonClassification.Junior;
-				break;
-			case "O":
-				classification = PersonClassification.Sophomore;
-				break;
-			case "F":
-			default:
-				classification = PersonClassification.Freshman;
-				break;
-		}
-		StudentService.Current.Add(new Student { Name = context.Name, Classification = classification });
-        Shell.Current.GoToAsync("//Instructor");
+		(BindingContext as PersonDetailViewModel).AddPerson();
+        
+    }
+
+    private void OnLeaving(object sender, NavigatedFromEventArgs e)
+    {
+		BindingContext = null;
+    }
+
+	private void OnArriving(object sender, NavigatedToEventArgs e) {
+        BindingContext = new PersonDetailViewModel();
     }
 }
